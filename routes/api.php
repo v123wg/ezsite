64a5da3b72181c9062740e02916fdd3e2ea92e10
佑安 簡 <a0963318069@gmail.com>
2021-06-04 09:52:27 +0800

    初始化

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Article;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+ */
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['namespace' => '\Javck\Ezlaravel\Http\Controllers'], function () {
+    Route::get('items/show', 'ApiController@showSingleItem');
+    Route::get('items/{item}', 'ApiController@queryItem');
+    //Official
+    Route::post('areas/queryByCounty', 'ApiController@queryAreas');
+    Route::post('elements/queryPositions', 'ApiController@queryPositions');
+    Route::post('elements/queryElementModes', 'ApiController@queryElementModes');
+});
+
+//Route::get('/posts',function(){
+//	$articles=Article::get();
+//	return $articles;
+//});
+
+//Route::get('/posts',function(){
+//$articles = Article::get();
+//return response()->json(['status' => 1, 'data' => $articles])->setEncodingOptions(JSON_UNESCAPED_UNICODE);
+//});
